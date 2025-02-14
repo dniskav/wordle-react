58d53ee3ec32e46fef2bf4abec814ce0195471ca
Dniskav <dniskav@gmail.com>
2025-02-14 22:15:55 +0100

    base config

---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="web-title">Rxjs Wordle</h1>
+      <div className="letters-container">
+        <div className="letter-row">
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+        </div>
+        <div className="letter-row">
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+        </div>
+        <div className="letter-row">
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+        </div>
+        <div className="letter-row">
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+        </div>
+        <div className="letter-row">
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+        </div>
+        <div className="letter-row">
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+        </div>
+        <div className="letter-row">
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+          <div className="letter"></div>
+        </div>
+        <div className="message-container">
+          <p className="message-text"></p>
+        </div>
+        <div className="restar-container">
+          <button className="restart-button">Restart</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
