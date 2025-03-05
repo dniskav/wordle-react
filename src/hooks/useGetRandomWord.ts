be9ed20e11641ceb8dcb6bb402e096c1ac0a3241
Dniskav <dniskav@gmail.com>
2025-03-05 11:26:25 +0100

    fix hardcoded code

--- src/hooks/useGetRandomWord.ts
+++ src/hooks/useGetRandomWord.ts
@@ -1,27 +1,30 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback } from 'react';
+const url: string = `${import.meta.env.BASE_URL}data/words.json`;
 
-const getRandomWord = (words: string[]): string => 
+const getRandomWord = (words: string[]): string =>
   words[Math.floor(Math.random() * words.length)];
 
 export default function useGetRandomWord() {
-  const [newWord, setNewWord] = useState<string>("");
+  const [newWord, setNewWord] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
   const fetchNewWord = useCallback(async () => {
     setLoading(true);
     setError(null);
-    
+
     try {
-      const response = await fetch("http://localhost:5173/data/words.json");
+      const response = await fetch(url);
       if (!response.ok) {
-        throw new Error(`Error al obtener las palabras: ${response.statusText}`);
+        throw new Error(
+          `Error al obtener las palabras: ${response.statusText}`,
+        );
       }
-      
+
       const data: string[] = await response.json();
       setNewWord(getRandomWord(data));
     } catch (err) {
-      setError(err instanceof Error ? err.message : "omg :|");
+      setError(err instanceof Error ? err.message : 'omg :|');
     } finally {
       setLoading(false);
     }
